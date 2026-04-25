9a5448d7a206ea57ed53a7975569bee294e143ad
kratikayadavv <kratikay68@gmail.com>
2026-04-25 14:25:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,23 @@
 import sqlite3
+def init_db():
+    conn = sqlite3.connect("animals_v3.db")
+    cursor = conn.cursor()
 
-conn = sqlite3.connect("animals_v3.db")
-cursor = conn.cursor()
-
-cursor.execute("DROP TABLE IF EXISTS cases")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS cases (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    animal TEXT,
-    location TEXT,
-    description TEXT,
-    image TEXT,
-    helper_name TEXT,
-    helper_contact TEXT        
+    cursor.execute("DROP TABLE IF EXISTS cases")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS cases (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        animal TEXT,
+        location TEXT,
+        description TEXT,
+        image TEXT,
+        helper_name TEXT,
+        helper_contact TEXT        
 )
 """)
 
 conn.commit()
 conn.close()
 
+init_db()
 print("Database created!")
